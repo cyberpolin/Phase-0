a734c88078912d1ec2ed33db960e8db400b3af46
Carlos Gonzalez <cyberpolin@gmail.com>
2016-03-03 18:31:08 -0600

    add reflection

---
+++ week-4/define-method/my_solution.rb
@@ -0,0 +1,8 @@
+# Define an Empty Method
+def do_something(ar1, ar2, ar3)
+end
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
--- week-4/variables-methods.rb
+++ week-4/variables-methods.rb
@@ -34,4 +34,25 @@ greet = Variables.new
 greet.greeting()
 greet.bigger_better()
 
-# Release 2 ending
+# How do you define a local variable?
+# => You just do it, the local variable is the easiest one to declare and use
+
+# How do you define a method?
+# => by typing def followed by the name of the method, then add at end. Everything in between is the block that will execute when we call the method.
+#    A method can have parameters or not, this last ones are declared between parethesis, or arfet the name of the method.
+
+# What is the difference between a local variable and a method?
+# => One is a variable, and the other is a way of asigning a label to a block of code, a method can have variables inside it self, and a variable can hold the result of a method.
+
+# How do you run a ruby program from the command line?
+# => ruby name_of_the_program.rb
+
+# How do you run an RSpec file from the command line?
+# => rspec name_of_the_spec_file.rb
+
+# What was confusing about this material? What made sense?
+# => the operators beeing methods, and the self object, everything else was perfect
+
+# Add a link to each exercise solution file as well so your peers can easily look at your code.
+# =>
+
