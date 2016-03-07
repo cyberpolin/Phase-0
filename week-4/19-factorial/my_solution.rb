f7e3198884908b0ace542c5e97f406fb40bac449
Carlos Gonzalez <cyberpolin@gmail.com>
2016-03-06 22:14:26 -0600

    donde

---
+++ week-4/19-factorial/my_solution.rb
@@ -0,0 +1,21 @@
+# Factorial
+
+# I worked on this challenge [with:Alec Hendrickson ].
+
+
+# Your Solution Below
+def factorial(number)
+  # Your code goes here
+  # Your code goes here
+  # Iter from 1 as i to number
+  total = 1
+  number.times do |x|
+    total = total * (x+1)
+  end
+  p total
+    # save the value of i in iter_var
+    # multiply iter_var times i
+    # sum iter_var times i in to total
+
+  #return total
+end
