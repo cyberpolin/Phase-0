d929cc9ef17476e78d1f27592d3f5dbd642c6aed
Carlos Gonzalez <cyberpolin@gmail.com>
2016-03-09 11:38:52 -0600

    done with 5.2

--- week-5/pad-array/doc/js/search_index.js
+++
@@ -1,1 +0,0 @@
-var search_data = {"index":{"searchIndex":["object","pad()","pad!()"],"longSearchIndex":["object","object#pad()","object#pad!()"],"info":[["Object","","Object.html","",""],["pad","Object","Object.html#method-i-pad","(array, min_size, value = nil)",""],["pad!","Object","Object.html#method-i-pad-21","(array, min_size, value = nil)","<p>Initial Solution\n\n"]]}}
