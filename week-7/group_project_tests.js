4a58067f0234360c9750a8678d2eee4276609169
Carlos Gonzalez <cyberpolin@gmail.com>
2016-03-27 16:15:59 -0600

    add code

--- week-7/group_project_tests.js
+++ week-7/group_project_tests.js
@@ -1,3 +1,49 @@
+// Pseudocode:
+
+
+
+// - create a function called sum
+// - add all the numbers in the passed array together, and return that sum
+
+function sum(int_array){
+  total = 0;
+  for (i in int_array){
+    total += int_array[i];
+  }
+  return total
+}
+// - create a function called mean
+// - add all the numbers of the passed array together
+// - divide the sum by the amount of numbers in the array to get the average
+// - return the average
+
+function mean(int_array){
+  total = sum(int_array);
+  return average = total/(int_array.length)
+}
+// - create a function called median
+// - sort the numbers in the passed array from low to high
+// - calculate the length of the array
+// IF
+//      the length of the array equals is odd
+//    - divide the length by 2 and create a variable that is equal to the result of the division rounded down to a whole number.
+//    - return the number in the array that is at the index of the variable's value.
+// ELSE
+//    - the length of the array is even.
+//    - divide the length of the array by 2 and return the number in the array at the index equal to the result of the division
+
+function median(int_array){
+  sorted_array = int_array.sort();
+  array_length = int_array.length;
+  if (array_length%2 == 1){
+    var median = Math.floor(array_length/2);
+    return int_array[median];
+  } else {
+    return int_array[array_length/2];
+
+  }
+}
+
 
 // Add the finished solution here when you receive it.
 // __________________________________________
