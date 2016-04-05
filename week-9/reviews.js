e3a5d7cd2c4fc94b189c0b880442990f6963772c
Carlos Gonzalez <cyberpolin@gmail.com>
2016-04-05 13:15:25 -0500

    finish review.js

--- week-9/reviews.js
+++ week-9/reviews.js
@@ -33,28 +33,85 @@
   // print the list to screen
 // =============
 
-list_items = {}
+// function List (name) {
+//   this.name =  name;
+//   this.list_items = {}
+//   console.log("You have succsesfully created a list named:" + this.name);
+//   this.add = function (item, value){
+//     if (value == undefined){// TO IMPROVE
+//       value = 1;
+//     }
+//     this.list_items[item] = value;
+//     //console.log(list_items)
+//   }
+//   this.remove = function (item){
+//     delete this.list_items[item];
+//     //console.log(list_items);
+//   }
+//   this.update = function(item, value){
+//     this.list_items[item] = value;
+//   }
+//   this.print = function(){
+//     console.log( "My "+ this.name + " list.")
+//     for (item in this.list_items){
+//       console.log(item + " : " + this.list_items[item]);
+//     }
+//   }
+// }
 
+
+// var myList = new List("Super Market");
+// myList.add("Eggs");
+// myList.add("Tomatoes", 3);
+// myList.add("Chiken", 4);
+// myList.add("Carrots", 10);
+// myList.remove("Eggs");
+// myList.update("Tomatoes", 5);
+// myList.print();
+
+
+
+// REFACTOR
 function List (name) {
   this.name =  name;
+  this.list_items = {}
   console.log("You have succsesfully created a list named:" + this.name);
   this.add = function (item, value){
-    if (value == undefined){// TO IMPROVE
+    if (value == undefined){// Tried to improve it, but it looks like it's not supported yet
       value = 1;
     }
-    list_items[item] = value;
-    console.log(list_items)
+    this.list_items[item] = value;
   }
   this.remove = function (item){
-    delete list_items.item;
-    console.log(list_items);
+    delete this.list_items[item];
+  }
+  this.update = function(item, value){
+    this.list_items[item] = value;
+  }
+  this.print = function(){
+    console.log( "My "+ this.name + " list.")
+    for (item in this.list_items){
+      console.log(item + " : " + this.list_items[item]);
+    }
   }
 }
 
 var myList = new List("Super Market");
 myList.add("Eggs");
+myList.add("Tomatoes", 3);
+myList.add("Chiken", 4);
+myList.add("Carrots", 10);
 myList.remove("Eggs");
-console.log(myList.constructor);
-console.log(myList.name);
+myList.update("Tomatoes", 5);
+myList.print();
 
 
+// REFLECTION
+// What concepts did you solidify in working on this challenge? (reviewing the passing of information, objects, constructors, etc.)
+  // How to create a prototype in javascript, and default parameters values.
+
+// What was the most difficult part of this challenge?
+  // To think it in OOP jejej
+
+// Did an array or object make more sense to use and why?
+  // The object is a better suitable variable since we have both pieces of information,
